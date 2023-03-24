623fab3ea52c24c346914fddbed433fbfe551d0e
WYH2004 <wangyuhao2004@gmail.com>
2023-03-24 19:07:24 +0800

    修复了JSONObject引发的报错

--- src/main/kotlin/BotCensor.kt
+++ src/main/kotlin/BotCensor.kt
@@ -1,20 +1,14 @@
 package moe.wyh2004
 
 import com.baidu.aip.contentcensor.AipContentCensor
-import kotlinx.serialization.json.JsonPrimitive
 import moe.wyh2004.command.BotCensorCommand
 import moe.wyh2004.config.DefaultConfig
+import moe.wyh2004.event.MsgPreSendCensor
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.GlobalEventChannel
-import net.mamoe.mirai.event.events.MessagePreSendEvent
-import net.mamoe.mirai.message.data.Image
-import net.mamoe.mirai.message.data.PlainText
-import net.mamoe.mirai.message.data.toMessageChain
 import net.mamoe.mirai.utils.info
-import org.json.JSONObject
-import java.util.*
 
 object BotCensor : KotlinPlugin(
     JvmPluginDescription(
@@ -28,42 +22,10 @@ object BotCensor : KotlinPlugin(
     lateinit var client:AipContentCensor
     override fun onEnable() {
         logger.info { "插件已加载" }
-        regCommand()
         regConfig()
         initBaiduAip()
-
-        GlobalEventChannel.subscribeAlways<MessagePreSendEvent> {
-            val lowerCaseMessage = message.toString().lowercase(Locale.getDefault())
-            if (!DefaultConfig.QQWhiteList.contains(target.id) && !DefaultConfig.MessageWhiteList.any { lowerCaseMessage.contains(it.lowercase(Locale.getDefault())) }){
-                for (msg in message.toMessageChain()) {
-                    var resultType = false
-                    val result = when (msg) {
-                        is PlainText -> AipCensor.textCensor(client,msg)
-                        is Image -> {
-                            if(DefaultConfig.ImageCensor){
-                                AipCensor.imageCensor(client,msg)
-                            }else{
-                                JSONObject(mapOf("conclusion" to JsonPrimitive("跳过")))
-                            }
-                        }
-                        else -> JSONObject(mapOf("conclusion" to JsonPrimitive("错误的消息类型")))
-                    }
-                    if (result.get("conclusion").equals("不合规")){
-                        resultType = true
-                        val reason = result.getJSONArray("data").getJSONObject(0).get("msg")
-                        if (DefaultConfig.EnableLogger){
-                            logger.info("机器人尝试输出:\n$message\n已被拦截,拦截原因:$reason")
-                        }
-                        message = if (DefaultConfig.BlockInfo) {
-                            PlainText(DefaultConfig.BlockMessage + "\n拦截原因:$reason")
-                        }else{
-                            PlainText(DefaultConfig.BlockMessage)
-                        }
-                    }
-                    if (resultType) break
-                }
-            }
-        }
+        regCommand()
+        regListener()
     }
 
     override fun onDisable() {
@@ -80,7 +42,11 @@ object BotCensor : KotlinPlugin(
         DefaultConfig.reload()
     }
 
-    fun regCommand(){
+    private fun regListener(){
+        GlobalEventChannel.registerListenerHost(MsgPreSendCensor)
+    }
+
+    private fun regCommand(){
         BotCensorCommand.register()
     }
 }
---
+++ src/main/kotlin/event/MsgPreSendCensor.kt
@@ -0,0 +1,56 @@
+package moe.wyh2004.event
+
+import moe.wyh2004.AipCensor
+import moe.wyh2004.BotCensor
+import moe.wyh2004.config.DefaultConfig
+import net.mamoe.mirai.event.EventHandler
+import net.mamoe.mirai.event.ListenerHost
+import net.mamoe.mirai.event.events.MessagePreSendEvent
+import net.mamoe.mirai.message.data.Image
+import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.toMessageChain
+import org.json.JSONException
+import java.util.*
+
+/**
+ * @author WYH2004
+ * @date 2023/3/24
+ **/
+object MsgPreSendCensor : ListenerHost {
+    @EventHandler
+    suspend fun MessagePreSendEvent.onEvent(){
+        val lowerCaseMessage = message.toString().lowercase(Locale.getDefault())
+        if (!DefaultConfig.QQWhiteList.contains(target.id) && !DefaultConfig.MessageWhiteList.any { lowerCaseMessage.contains(it.lowercase(
+                Locale.getDefault())) }){
+            for (msg in message.toMessageChain()) {
+                var resultType = false
+                val result = when (msg) {
+                    is PlainText -> AipCensor.textCensor(BotCensor.client,msg)
+                    is Image -> {
+                        if(DefaultConfig.ImageCensor){
+                            AipCensor.imageCensor(BotCensor.client,msg)
+                        }else break
+                    }
+                    else -> break
+                }
+                try {
+                    if (result.get("conclusion").equals("不合规")){
+                        resultType = true
+                        val reason = result.getJSONArray("data").getJSONObject(0).get("msg")
+                        if (DefaultConfig.EnableLogger){
+                            BotCensor.logger.info("机器人尝试输出:\n$message\n已被拦截,拦截原因:$reason")
+                        }
+                        message = if (DefaultConfig.BlockInfo) {
+                            PlainText(DefaultConfig.BlockMessage + "\n拦截原因:$reason")
+                        }else{
+                            PlainText(DefaultConfig.BlockMessage)
+                        }
+                    }
+                }catch (e : JSONException){
+                    BotCensor.logger.error("百度API返回数据有误，该消息未经过审核")
+                }
+                if (resultType) break
+            }
+        }
+    }
+}
